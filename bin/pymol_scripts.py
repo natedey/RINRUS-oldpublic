092bb20ec530655e9e83c32bb5380656f2d8c4d6
Qianyi Cheng <qcheng1@log001.cm.cluster>
2019-06-12 12:10:07 -0500

    add write_input.py

---
+++ bin/pymol_scripts.py
@@ -0,0 +1,25 @@
+"""
+This is a program written by qianyi cheng in deyonker research group
+at university of memphis.
+"""
+import os, sys
+
+def system_run(cmd):
+    print(cmd)
+    exit = os.system(cmd)
+    if ( exit != 0 ):
+        print('failed to run:')
+        print(cmd)
+        sys.exit()
+
+input = sys.argv[1]
+name  = input.split('.')[0]
+ouput = name+'_h.pdb'
+logf = open('log.pml','w')
+if len(sys.argv) == 3:
+    logf.write('load %s\ncmd.select("sel","%s and not resi %s")\ncmd.h_add("sel")\ncmd.save("./%s")'%(input,name,sys.argv[2],ouput))
+elif len(sys.argv) == 2:
+    logf.write('load %s\ncmd.h_add("%s")\ncmd.save("./%s")'%(input,name,ouput))
+else:
+    print("Something is wrong!")
+print("Please run 'pymol -qc log.pml'")
