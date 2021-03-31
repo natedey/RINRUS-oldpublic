d59c4d5755be211b668173990947dddee04d95e7
tejaskumar suhagia <55853616+tsuhagia@users.noreply.github.com>
2021-03-31 17:59:47 -0500

    Add files via upload

--- examples/mesh/probe_mesh_rin.py
+++ examples/mesh/probe_mesh_rin.py
@@ -35,12 +35,12 @@ with args.model as m:
 keymodel=[]
 for line in mlines:
     record = line[:6].strip()
-    serial = ( line[6:11].strip() )
+    serial =  line[6:11].strip() 
     atomname = line[12:16].strip()
     altloc = line[16].strip()
     resname = line[17:20].strip()
     chain = line[21].strip()
-    resnum = ( line[22:26].strip() )
+    resnum =  line[22:26].strip() 
     achar = line[26].strip()
     x = float(line[30:38])
     y = float(line[38:46])
@@ -63,9 +63,9 @@ with open (f"{writeto}","w") as writestart:
         c = line.split(':')
         obj1 = c[1]
         acts = c[2]
-        xcordinate= c[-5]
-        ycordinate= c[-4]
-        zcordinate= c[-3]
+        xcordinate= float(c[-5])
+        ycordinate= float(c[-4])
+        zcordinate= float(c[-3])
         atom= c[-7]
         score1= c[-9]
         score2= c[-8]
@@ -76,26 +76,22 @@ with open (f"{writeto}","w") as writestart:
         key1 = cha1+':'+res_id1+':'+res_nm1+':'+atom1
         key2 = cha2+':'+res_id2+':'+res_nm2+':'+atom2
 
-        if key1 in keymodel:
+        if key1 in keymodel and key2 not in keymodel:
             if (acts == "hb"):
-                printlist=["H",xcordinate,ycordinate,zcordinate,"@",acts,key2,score1,score2]
+                writestart.write("%-6s %8.3f %8.3f %8.3f %6s %4s %15s %15s %8s %8s\n"%("H",xcordinate,ycordinate,zcordinate,"@",acts,key2,key1,score1,score2))
             elif (acts == "bo" or acts == "so"):
-                printlist=["N",xcordinate,ycordinate,zcordinate,"@",acts,key2,score1,score2] 
+                writestart.write("%-6s %8.3f %8.3f %8.3f %6s %4s %15s %15s %8s %8s\n"%("N",xcordinate,ycordinate,zcordinate,"@",acts,key2,key1,score1,score2))
             else:
-                printlist=["O",xcordinate,ycordinate,zcordinate,"@",acts,key2,score1,score2]
-            list_to_string='   '.join((printlist)) 
-            writestart.writelines('%s\n' % list_to_string)
+                writestart.write("%-6s %8.3f %8.3f %8.3f %6s %4s %15s %15s %8s %8s\n"%("O",xcordinate,ycordinate,zcordinate,"@",acts,key2,key1,score1,score2))
             linecount += 1
 
-        if key2 in keymodel:
+        if key2 in keymodel and key1 not in keymodel:
             if (acts == "hb"):
-                printlist=["H",xcordinate,ycordinate,zcordinate,"@",acts,key1,score1,score2]
+                writestart.write("%-6s %8.3f %8.3f %8.3f %6s %4s %15s %15s %8s %8s\n"%("H",xcordinate,ycordinate,zcordinate,"@",acts,key1,key2,score1,score2))
             elif (acts == "bo" or acts == "so"):
-                printlist=["N",xcordinate,ycordinate,zcordinate,"@",acts,key1,score1,score2] 
+                writestart.write("%-6s %8.3f %8.3f %8.3f %6s %4s %15s %15s %8s %8s\n"%("N",xcordinate,ycordinate,zcordinate,"@",acts,key1,key2,score1,score2))
             else:
-                printlist=["O",xcordinate,ycordinate,zcordinate,"@",acts,key1,score1,score2]
-            list_to_string='   '.join((printlist)) 
-            writestart.writelines('%s\n' % list_to_string)
+                writestart.write("%-6s %8.3f %8.3f %8.3f %6s %4s %15s %15s %8s %8s\n"%("O",xcordinate,ycordinate,zcordinate,"@",acts,key1,key2,score1,score2))
             linecount += 1
 #######################################################################
 ## Next Part count atoms and create xyz format file from all cordinates.
@@ -103,13 +99,73 @@ with open (f"{writeto}","w") as writestart:
 
 readstart= open(f"{writeto}")
 xyzlines= readstart.readlines()
-
 with open (f"{meshxyz}","w") as writexyzstart:
     writexyzstart.writelines(str('%s\n' % linecount))
     writexyzstart.writelines(str('%s\n' % (" ")))
     for xline in xyzlines:
-        xline1,xline2= xline.split('@')
-        writexyzstart.writelines('%s\n' %xline1)
+        writexyzstart.writelines('%s\n' %xline[:37])
+
+with open ("probe_mesh000.dat","w") as writexyz000start:
+    for line0 in xyzlines:
+        if re.search("0.000", line0):
+            writexyz000start.writelines(line0)
+with open ("probe_mesh000.xyz","w") as writexyznxstart:
+    writexyznxstart.writelines(str('%s\n' % (sum(1 for line in open("probe_mesh000.dat")))))
+    writexyznxstart.writelines(str('%s\n' % (" ")))
+    for xline in open("probe_mesh000.dat","r").readlines():
+        writexyznxstart.writelines('%s\n' %xline[:37]) 
+
+with open ("probe_meshXXX.dat","w") as writexyzXXXstart:
+    for lineX in xyzlines:
+        if re.search("0.000", lineX):
+            pass
+        else:
+            writexyzXXXstart.writelines(lineX)
+with open ("probe_meshXXX.xyz","w") as writexyznxstart:
+    writexyznxstart.writelines(str('%s\n' % (sum(1 for line in open("probe_meshXXX.dat")))))
+    writexyznxstart.writelines(str('%s\n' % (" ")))
+    for xline in open("probe_meshXXX.dat","r").readlines():
+        writexyznxstart.writelines('%s\n' %xline[:37]) 
+
+with open ("probe_meshCA.dat","w") as writexyzCAstart:
+    for lineCA in xyzlines:
+        if re.search("CA", lineCA[75:77]):
+            writexyzCAstart.writelines(lineCA)
+with open ("probe_meshCA.xyz","w") as writexyznxstart:
+    writexyznxstart.writelines(str('%s\n' % (sum(1 for line in open("probe_meshCA.dat")))))
+    writexyznxstart.writelines(str('%s\n' % (" ")))
+    for xline in open("probe_meshCA.dat","r").readlines():
+        writexyznxstart.writelines('%s\n' %xline[:37]) 
+
+with open ("probe_meshH.dat","w") as writexyzHstart:
+    for lineH in xyzlines:
+        if re.search("H", lineH[0]):
+            writexyzHstart.writelines(lineH)
+with open ("probe_meshH.xyz","w") as writexyznxstart:
+    writexyznxstart.writelines(str('%s\n' % (sum(1 for line in open("probe_meshH.dat")))))
+    writexyznxstart.writelines(str('%s\n' % (" ")))
+    for xline in open("probe_meshH.dat","r").readlines():
+        writexyznxstart.writelines('%s\n' %xline[:37]) 
+
+with open ("probe_meshN.dat","w") as writexyzNstart:
+    for lineN in xyzlines:
+        if re.search("N", lineN[0]):
+            writexyzNstart.writelines(lineN)    
+with open ("probe_meshN.xyz","w") as writexyznxstart:
+    writexyznxstart.writelines(str('%s\n' % (sum(1 for line in open("probe_meshN.dat")))))
+    writexyznxstart.writelines(str('%s\n' % (" ")))
+    for xline in open("probe_meshN.dat","r").readlines():
+        writexyznxstart.writelines('%s\n' %xline[:37])       
+
+with open ("probe_mesho.dat","w") as writexyzostart:
+    for lineo in xyzlines:
+        if re.search("O", lineo[0]):
+            writexyzostart.writelines(lineo)
+with open ("probe_mesho.xyz","w") as writexyznxstart:
+    writexyznxstart.writelines(str('%s\n' % (sum(1 for line in open("probe_mesho.dat")))))
+    writexyznxstart.writelines(str('%s\n' % (" ")))
+    for xline in open("probe_mesho.dat","r").readlines():
+        writexyznxstart.writelines('%s\n' %xline[:37])  
 
 print(f"xyz cordinates saved in {writeto} and saving xyz molecule format in {meshxyz} is completed.\n H = hb (hydrogen bond), N = so,bo (big overlap), o = others ")
 writestart.close()
